cd2be3703736105897a3e55e7493c83b0cbdcc77
ndiazcalens <ndiazcalens@gmail.com>
2025-05-10 03:32:21 -0300

    Cambiando logica carrito

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { FaCartShopping } from "react-icons/fa6";
 
 function Cart(){
     return(
-        <h1>esto es cart</h1>
+        <>
+            <label className="cart-btn" htmlFor="cart-btn">
+                <FaCartShopping />
+            </label>
+            <input type="checkbox" hidden/>
+
+            <aside className="cart">
+                <ul>
+                    <li>nombre</li>
+                    <li>foto</li>
+                    <li>precio</li>
+                    <li>cantidad</li>
+                </ul>
+            </aside>
+
+        </>
     )
 }
 
